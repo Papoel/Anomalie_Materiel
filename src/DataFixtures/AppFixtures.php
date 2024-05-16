22f19a190e017a1ff043cc2e9e7a03f0df659705
Papoel <pascal.briffard@gmail.com>
2024-05-16 17:45:33 +0200

    WIP: Entity User + Fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        $admin = new User();
+        $admin->setNni(nni: 'F07583');
+        $admin->setFirstname(firstname: 'Pascal');
+        $admin->setLastname(lastname: 'Briffard');
+        $admin->setRoles(roles: ['ROLE_ADMIN']);
+        $hash = $this->passwordHasher->hashPassword(user: $admin, plainPassword: 'admin');
+        $admin->setPassword(password: $hash);
+
+        $manager->persist($admin);
+
+        // Generate 150 users
+        for ($i = 1; $i <= 50; $i++) {
+            $user = new User();
+            // Random NNI : start with letter between A and Z and 5 digits
+            $user->setNni(nni: $faker->regexify(regex: '/[A-Z]\d{5}/'));
+            $user->setFirstname(firstname: $faker->firstName());
+            $user->setLastname(lastname: '$faker->lastName()');
+            $user->setRoles(roles: ['ROLE_USER']);
+            $hash = $this->passwordHasher->hashPassword(user: $user, plainPassword: 'user');
+            $user->setPassword(password: $hash);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Stringable;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`users`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NNI', fields: ['nni'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface, stringable
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $nni = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $lastname = null;
+
+    public function __toString(): string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName() . ' (' . $this->getNni() . ')';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNni(): ?string
+    {
+        return $this->nni;
+    }
+
+    public function setNni(string $nni): static
+    {
+        $this->nni = $nni;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->nni;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): static
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): static
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+}
