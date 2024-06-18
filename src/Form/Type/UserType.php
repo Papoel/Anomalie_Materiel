f918365f6d7e5858a2a0bc99beb5c849ef5659f6
Papoel <pascal.briffard@gmail.com>
2024-06-18 22:31:53 +0200

    add form creation User

---
+++ src/Form/Type/UserType.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(child: 'firstname', type: TextType::class, options: [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Prénom',
+                ],
+                'required' => false,
+            ])
+            ->add(child: 'lastname', type: TextType::class, options: [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Nom',
+                ],
+                'required' => false,
+            ])
+            ->add(child: 'nni', type: TextType::class, options: [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'NNI',
+                ],
+                'required' => false,
+            ])
+            ->add(child: 'roles', type: CollectionType::class, options: [
+                'label' => false,
+                'entry_type' => ChoiceType::class,
+                'entry_options' => [
+                    'label' => false,
+                    'choices' => [
+                        'Utilisateur' => 'ROLE_USER',
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Chargé d\'affaires' => 'ROLE_CHARGE_AFFAIRES',
+                        'Méthode' => 'ROLE_METHODE',
+                    ],
+                ],
+                'required' => false,
+            ])
+            ->add(child: 'password', type: RepeatedType::class, options: [
+                'type' => PasswordType::class,
+                'required' => false,
+                'first_options' => [
+                    'label' => false,
+                    'always_empty' => false, // Permet de ne pas afficher le champ 'Mot de passe' si le champ 'Mot de passe' est vide
+                    'attr' => [
+                        'placeholder' => 'Mot de passe',
+                    ],
+                    'required' => false,
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'always_empty' => false, // Permet de ne pas afficher le champ 'Répéter le mot de passe' si le champ 'Mot de passe' est vide
+                    'attr' => [
+                        'placeholder' => 'Répéter le mot de passe',
+                    ],
+                    'required' => false,
+                ],
+            ])
+            ->add(child: 'email', type: EmailType::class, options: [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Email',
+                ],
+                'required' => false,
+            ])
+            ->add(child: 'submit', type: SubmitType::class, options: [
+                'label' => 'Créer un compte',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'attr' => ['novalidate' => 'novalidate', 'html5' => false],
+        ]);
+    }
+}
---
+++ src/Twig/Components/UserForm.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Entity\User;
+use App\Form\Type\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\ValidatableComponentTrait;
+
+#[AsLiveComponent]
+final class UserForm extends AbstractController
+{
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+    use ValidatableComponentTrait;
+
+    #[LiveProp]
+    public ?User $initialFormData = null;
+
+    protected function instantiateForm(): FormInterface
+    {
+        $this->initialFormData ??= new User();
+
+        return $this->createForm(type: UserType::class, data: $this->initialFormData);
+    }
+
+    #[LiveAction]
+    public function save(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // Valider le formulaire
+        $this->validate();
+
+        // Si le formulaire est valide, on le soumet
+        $this->submitForm();
+
+        // Vérifie si le formulaire n'est pas null avant d'appeler getData()
+        if (null === $this->form) {
+            throw new \RuntimeException(message: 'Le formulaire n\'a pas été initialisé.');
+        }
+
+        /** @var User $user */
+        $user = $this->form->getData();
+
+        // Hasher le mot de passe
+        $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Si le formulaire est valide, on redirige vers la page d'accueil
+        return $this->redirectToRoute(route: 'user_show', parameters: [
+            'nni' => $user->getNni(),
+        ]);
+    }
+}
