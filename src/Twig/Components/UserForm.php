aeace1a1dba9d42d8a0d7d1c348902e5d0cb28bf
Papoel <pascal.briffard@gmail.com>
2024-06-25 17:03:19 +0200

    refactor: update namespace, improve password handling and data logic in UserForm

--- src/Twig/Components/UserForm.php
+++ src/Twig/Components/UserForm.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
-namespace App\Twig;
+namespace App\Twig\Components;
 
 use App\Entity\User;
 use App\Form\Type\UserType;
+use App\Service\User\UserPasswordHasherService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
@@ -26,18 +26,17 @@ final class UserForm extends AbstractController
     use ValidatableComponentTrait;
 
     #[LiveProp]
-    public ?User $initialFormData = null;
+    public ?User $data = null;
 
     protected function instantiateForm(): FormInterface
     {
-        $this->initialFormData ??= new User();
-        // Rendre le nom.prenom dans email
-        $this->initialFormData->setEmail($this->initialFormData->getFirstname() . '.' . $this->initialFormData->getLastname(). '@edf.fr');
+        $this->data ??= new User();
 
-        return $this->createForm(type: UserType::class, data: $this->initialFormData);
+        return $this->createForm(type: UserType::class, data: $this->data);
     }
+
     #[LiveAction]
-    public function save(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    public function save(EntityManagerInterface $entityManager, UserPasswordHasherService $passwordHasher): Response
     {
         // Valider le formulaire
         $this->validate();
@@ -53,16 +52,40 @@ final class UserForm extends AbstractController
         /** @var User $data */
         $data = $this->form->getData();
 
-        // Hasher le mot de passe
-        $hashedPassword = $passwordHasher->hashPassword($data, $data->getPassword());
-        $data->setPassword($hashedPassword);
+        $plainPassword = $data->getPassword();
+        if ('' !== $plainPassword) {
+            $hashedPassword = $passwordHasher->hashPassword($data, $plainPassword);
+            if (null !== $hashedPassword) {
+                $data->setPassword($hashedPassword);
+            }
+        }
 
         $entityManager->persist($data);
-        $entityManager->flush();
 
-        // Si le formulaire est valide, on redirige vers la page d'accueil
+        // Before the flush, we need verify if the NNI is unique in the database
+        $nni = $data->getNni();
+
+        if ($entityManager->getRepository(User::class)->findOneBy(['nni' => $nni])) {
+            $this->addFlash(type: 'danger', message: "Désolé, le NNI \"$nni\" est déjà attribué à un autre utilisateur.");
+
+            return $this->redirectToRoute(route: 'home_index');
+        }
+
+        if ($entityManager->getRepository(User::class)->findOneBy(['email' => $data->getEmail()])) {
+            $this->addFlash(type: 'danger', message: "Désolé, l'adresse email \"{$data->getEmail()}\" est déjà attribuée à un autre utilisateur.");
+
+            return $this->redirectToRoute(route: 'home_index');
+        }
+
+        $entityManager->flush();
+        $firstName = $data->getFirstname();
+        $lastName = $data->getLastname();
+
+        $this->addFlash(type: 'success', message: "L'utilisateur \"$firstName $lastName\" a été créé avec succès.");
+
+        // Si le formulaire est valide, on redirige vers la page de l'utilisateur créé
         return $this->redirectToRoute(route: 'user_show', parameters: [
-            'nni' => $data->getNni(),
+            'id' => $data->getId(),
         ]);
     }
 }
