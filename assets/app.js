6391436f1471b56df1f046979565e3171a5d457b
Papoel <pascal.briffard@gmail.com>
2024-06-28 13:21:36 +0200

    style: remove fontawesome and update style

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,6 @@ import './vendor/bootstrap/dist/css/bootstrap.min.css';
 // Librairie Externe
 import './vendor/animate/animate.min.css';
 import './vendor/animate/animate.index.js';
-import  './vendor/fontawesome/css/all.min.css';
 import './vendor/bootstrap-icons/font/bootstrap-icons.min.css';
 
 // Stimulus
